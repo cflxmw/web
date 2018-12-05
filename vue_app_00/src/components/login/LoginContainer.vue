83a4001d125c2aa72a2a8601fd4e6673ead19099
dongwentao <790264634@qq.com>
2018-12-05 19:54:48 +0800

    vue-ui
    hhh

---
+++ vue_app_00/src/components/login/LoginContainer.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <header class="mui-bar mui-bar-nav">
+		<h1 class="mui-title">登录</h1>
+	</header>
+	<div class="mui-content">
+		<form id='login-form' class="mui-input-group">
+			<div class="mui-input-row">
+				<label>账号</label>
+				<input id='account' type="text" class="mui-input-clear mui-input" placeholder="请输入账号" name="uname" v-model="unameval">
+			</div>
+			<div class="mui-input-row">
+				<label>密码</label>
+				<input id='password' type="password" class="mui-input-clear mui-input" placeholder="请输入密码" name="upwd" v-model="upwdval">
+			</div>
+		</form>
+		<form class="mui-input-group">
+			<ul class="mui-table-view mui-table-view-chevron">
+				<li class="mui-table-view-cell">
+						自动登录
+					<div id="autoLogin" class="mui-switch">
+						<div class="mui-switch-handle"></div>
+					</div>
+				</li>
+			</ul>
+		</form>
+		<div class="mui-content-padded">
+			<button id='login' type="button" class="mui-btn mui-btn-bc 	lock mui-btn-primary" @click="btnSubmit">登录</button>
+			<div class="link-area"><a id='reg'>注册账号</a> <span class="spliter">|</span> <a id='forgetPassword'>忘记密码</a>
+			</div>
+			</div>
+		<div class="mui-content-padded oauth-area">
+
+		</div>
+	</div>
+  </div>
+</template>
+<script>
+  import {Toast} from "mint-ui";
+  export default {
+      data(){
+          return{
+						unameval:"",
+						upwdval:""
+					}
+      },
+			methods:{
+				btnSubmit(){
+					var u=this.unameval;
+					var p=this.upwdval;
+					this.$http.get("login?uname="+u+"$upwd="+p).then(result=>{
+            if(result.body.code == 1){
+							Toast(result.body.msg);
+							this.$router.push("/");
+						}else{
+							Toast(result.body.msg);
+						}
+					})
+				}
+			}
+  }
+</script>
+<style>
+</style>
---
+++ vue_app_00/src/components/tabbar/ShopContainer.vue
@@ -0,0 +1,102 @@
+<template>
+ <div class="app-shop">
+    <!--1:图片轮播-->
+    <div class="mui-card">
+				<div class="mui-card-content">
+					<div class="mui-card-content-inner">
+						<swiper-box :list="imagelist"></swiper-box>
+					</div>
+				</div>
+			</div>
+    <!--2:商品列表-->
+ 			<div class="mui-card">
+				<div class="mui-card-header">商品列表</div>
+				<div class="mui-card-content">
+					<div class="mui-card-content-inner">
+            <!--2.1mui 左侧图片 右侧文字-->
+          <ul class="mui-table-view">
+				   <li class="mui-table-view-cell mui-media" v-for="item in cartlist" :key="item.id">
+					  <a href="javascript:;">
+						 <img class="mui-media-object mui-pull-left" src="">
+						  <div class="mui-media-body">
+							  {{item.title}}
+							<p class='mui-ellipsis'>
+                 <span class="price">{{item.price}}</span>
+                 <span class="count">
+                   <!--2.2mui 数字按钮-->
+             <div class="mui-numbox" data-numbox-min='1' data-numbox-max='9'>
+					  <button class="mui-btn mui-btn-numbox-minus" type="button" @click="cartSub(item.id)">-</button>
+					   <input id="test" class="mui-input-numbox" type="number" :value="item.count" />
+					   <button class="mui-btn mui-btn-numbox-plus" type="button" @click="cartAdd(item.id)">+</button>
+				        </div>
+               </span>
+              </p>
+						</div>
+					 </a>
+				  </li>
+         </ul>
+				  <div>合计:{{getSubTotal}}</div>
+				</div>
+			</div>
+		</div>   
+ </div>
+</template>
+<script>
+  import swiper from '../sub/swiper.vue';
+  export default {
+   data(){
+     return {
+			 imagelist:[],
+			 cartlist:[]
+		 }
+   },
+	 methods:{
+		 cartSub(id){
+       for(var item of this.cartlist){
+				 if(item.id == id){
+					 if(item.count<2)
+					 return;
+					 item.count--;
+					 break;
+				 }
+			 }
+		 },
+		 cartAdd(id){
+       for(var item of this.cartlist){
+				 if(item.id == id){
+					 item.count++;
+					 break;
+				 }
+			 }
+		 },
+		 getCartList(){
+			 this.$http.get("shopCart").then(result=>{
+				 this.cartlist = result.body;
+			 })
+		 },
+		 getImageList(){
+			 this.$http.get("imagelist").then(result=>{
+				 this.imagelist = result.body;
+			 })
+		 }
+	 },
+   created(){
+		 this.getImageList();
+		 this.getCartList();
+   },
+	 components:{
+		 "swiper-box":swiper
+	 },
+	 computed:{
+		 getSubTotal:function(){
+			 var sum = 0;
+			 for(var item of this.cartlist){
+				 sum+=item.price * item.count;
+			 }
+			  return sum;
+		 }
+	 }
+ }
+</script>
+<style>
+</style>
